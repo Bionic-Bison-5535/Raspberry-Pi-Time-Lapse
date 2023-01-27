89d8ec7837b3b7c218bd434d981bb08e28d3cea4
Wesley McGinn <wesleymcginn1@gmail.com>
2023-01-27 15:24:01 -0500

    Removed 404 redirect
    When a file is not found, the user will not be redirected to the main page.  Rather, a 404 message will appear and explain simply that the file was not found.

--- server.js
+++ server.js
@@ -22,19 +22,8 @@ http.createServer((req, res) => {
 http.createServer((req, res) => {
   fs.readFile(timeLapseDirectory + req.url, function (err, data) {
     if (err) {
-      fs.readFile("/home/pi/index.html", function (err, data) {
-        if (err) {
-          console.log("404: HTML file does not exist or is not where it needs to be!");
-          console.log("Make sure that index.html is located in the /home/pi directory.");
-          console.log();
-          res.writeHead(404, {'Content-Type': 'text/html'});
-          return res.end("<body style='text-align: center'><h1><b><u>404 Error</u></b></h1><h2>Page not found.  :(<br>If you are the server owner, please ensure that index.html is located in the /home/pi directory.</h2></body>");
-        } else {
-          res.writeHead(200, {'Content-Type': 'text/html'});
-          res.write(data);
-          return res.end();
-        }
-      });
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("<body style='text-align: center'><h1><b><u>404 Error</u></b></h1><h2>The image you have requested does not exist.  :(</h2></body>");
     } else {
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.write(data);
