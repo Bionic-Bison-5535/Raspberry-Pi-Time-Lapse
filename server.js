18594faa246427df52d68ffb5153606d973d482e
Wesley McGinn <wesleymcginn1@gmail.com>
2023-02-10 16:37:35 -0500

    Removed Websocket
    Websocket was not needed, it took up more CPU, node.js would not update to use it as node is in version 0.

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const fs = require('fs');
 const http = require('http');
-const ws = require('ws');
-const wss = new ws.Server({noServer: true});
 
 var timeLapseDirectory = "/home/pi/Desktop";
-var enableFileRights = true;
 
 http.createServer(function (req, res) {
   fs.readFile("/home/pi/index.html", function (err, data) {
@@ -34,75 +31,3 @@ http.createServer(function (req, res) {
     }
   });
 }).listen(7777);
-
-http.createServer(function (req, res) {
-  wss.handleUpgrade(req, req.socket, Buffer.alloc(0), function onSocketConnect(ws) {
-    ws.on('message', function(message) {
-      var DATA = "" + message;
-      var DATAlist = eval(DATA);
-      if (DATAlist[0] == "getMostRecent") {
-        ws.send(mostRecentPhotoNumber(DATAlist[1], DATAlist[2]));
-      }
-      if (DATAlist[1] == "writeToFile") {
-        if (DATAlist[2] == "𝚙𝚊𝚜𝚜𝚠𝚘𝚛𝚍 ") {
-          writeToFile(DATAlist[3], DATAlist[4]);
-        } else {
-          console.log("HACKER ALERT! Someone incorrectly guessed the password for file writing to be:  " + DATAlist[2].toString());
-          enableFileRights = false;
-          setTimeout(function() {
-            enableFileRights = true;
-          }, 3000000);
-          console.log("File writing has been shut down for the next 50 minutes.");
-          console.log("");
-        }
-      }
-    });
-  });
-}).listen(8001);
-
-function four(Num) {
-    var newNum = Num;
-    while (newNum.length < 4) {
-        newNum = "0" + newNum;
-    }
-    return newNum;
-}
-
-function mostRecentPhotoNumber(basePhotoName, folderOfPhoto) {
-  var i = 0;
-  var thisWorked = false;
-  while (thisWorked == false && i < 10000) {
-    fs.readFile(timeLapseDirectory + "/" + folderOfPhoto + "/" + basePhotoName + four(i.toString()) + ".jpg", function (err, data) {
-      if (err) {
-        i++;
-      } else {
-        thisWorked = true;
-      }
-    });
-  }
-  if (thisWorked) {
-    return i;
-  } else {
-    return 0;
-  }
-}
-
-function writeToFile(fileDirectory, fileNewData) {
-  if (enableFileRights) {
-    fs.writeFile(fileDirectory, fileNewData, function(err) {
-      if (err) {
-        console.log("An error occured while trying to save a file: ");
-        console.log(err);
-        console.log("");
-      } else {
-        console.log("File updated: " + fileDirectory);
-        console.log("");
-      }
-    }); 
-  } else {
-    console.log("Someone tried to write to a file while file writing was disabled!  Only the disabler of file rights was there to protect your system!");
-    console.log("  -  Attempted to access: " + fileDirectory);
-    console.log("  -  Attempted to write:  " + fileNewData);
-    console.log("");
-  }
-}
