4ed168dd4fe87d7811d358d8a3f41f20f128ce7b
Wesley McGinn <wesleymcginn1@gmail.com>
2024-01-22 11:23:41 -0500

    Update server.js
    `server.js` has been updated for greater functionality and control.

--- server.js
+++ server.js
@@ -1,33 +1,98 @@
 const fs = require('fs');
 const http = require('http');
+const shell = require('shelljs');
 
-var timeLapseDirectory = "/home/pi/Desktop";
+const directory = "/home/pi/Desktop/Timelapse";
+var RES;
+
+var normalDays = {
+  list : ["Mon", "Tue", "Wed", "Thu", "Fri"],
+  startHour : 17,
+  startMin : 0,
+  endHour : 20,
+  endMin : 0
+};
+var otherDays = {
+  list : ["Sat"],
+  startHour : 8,
+  startMin : 30,
+  endHour : 15,
+  endMin : 30
+}
+
+var time = {
+  hour : parseInt(Date.call().slice(16,18)),
+  minute : parseInt(Date.call().slice(19,21)),
+  get : 60*parseInt(Date.call().slice(16,18)) + parseInt(Date.call().slice(19,21)),
+  a : function(obj) {
+    return (60*obj.startHour + obj.startMin);
+  },
+  b : function(obj) {
+    return (60*obj.endHour + obj.endMin);
+  },
+  name : function() {
+    return (Date.call().slice(4, 21)).replaceAll(" ","_");
+  }
+}
+
+function photo(fileName = "") {
+  if (fileName == "") {
+    shell.exec("raspistill -o " + directory + "/" + time.name() + ".jpg");
+  } else {
+    shell.exec("raspistill -o " + directory + "/" + fileName + ".jpg");
+  }
+}
+
+var running = setInterval(function() {
+  if ((normalDays.list.includes(Date.call().slice(0,3)) && time.get() > time.a(normalDays) && time.get() < time.b(normalDays)) || (otherDays.list.includes(Date.call().slice(0,3)) && time.get() > time.a(otherDays) && time.get() < time.b(otherDays))) {
+    photo();
+  }
+}, 60000);
 
 http.createServer(function (req, res) {
-  fs.readFile("/home/pi/index.html", function (err, data) {
-    if (err) {
-      console.log("404: HTML file does not exist or is not where it needs to be!");
-      console.log("Make sure that index.html is located in the /home/pi directory.");
-      console.log();
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("<body style='text-align: center'><h1 style='color: aqua'><b><u>404</u></b></h1><h2>File not found.<br><b>:(</b><br>If you are the server owner, please ensure that index.html is located in the /home/pi directory.</h2></body>");
+  var dir = req.url;
+  if (dir == "live") {
+    photo("live");
+    res.writeHead(202);
+    RES = res;
+    setTimeout(function() {
+      fs.readFile(directory + "/live.jpg", function (err, data) {
+        if (err) {
+          RES.writeHead(204);
+          RES.end();
+        } else {
+          RES.writeHead(200);
+          RES.end(data);
+        }
+      });
+    }, 2000);
+  } else {
+    if (dir.length < 3) {
+      dir = "/home/pi/index.html";
     } else {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      return res.end();
+      dir = directory + dir;
     }
-  });
+    fs.readFile(dir, function (err, data) {
+      if (err) {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.end("<body style='text-align:center;background-color:black'><h1 style='color:cyan'><b>404</b></h1><h2 style='color:red'>File not found.</h2><h1 style='font-size:200px;color:white'>):</h1></body>");
+      } else {
+        res.writeHead(200);
+        res.end(data);
+      }
+    });
+  }
+  if (dir.indexOf('#') != -1) {
+    if (dir.toLowerCase().indexOf("#stop") != -1) {
+      clearInterval(running);
+    }
+    if (dir.toLowerCase().indexOf("#start") != -1) {
+      clearInterval(running);
+      var running = setInterval(function() {
+        if ((normalDays.list.includes(Date.call().slice(0,3)) && time.get() > time.a(normalDays) && time.get() < time.b(normalDays)) || (otherDays.list.includes(Date.call().slice(0,3)) && time.get() > time.a(otherDays) && time.get() < time.b(otherDays))) {
+          photo();
+        }
+      }, 60000);
+    }
+  }
 }).listen(8000);
-
-http.createServer(function (req, res) {
-  fs.readFile(timeLapseDirectory + req.url, function (err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("<body style='text-align: center'><h1 style='color: aqua'><b><u>404</u></b></h1><h2>File not found.<br><b>:(</b></h2></body>");
-    } else {
-      res.writeHead(200);
-      res.write(data);
-      return res.end();
-    }
-  });
-}).listen(7777);
